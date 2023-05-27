2c4c966b1c830091d9664856ea2ecf7f5233afbb
won <whddnjsdlrk@naver.com>
2023-05-27 22:20:38 +0900

    fetch

--- login-lecture/app/src/public/js/home/login.js
+++ login-lecture/app/src/public/js/home/login.js
@@ -7,5 +7,16 @@ const id = document.querySelector("#id"),
 loginbtn.addEventListener('click',login);
 
 function login(){
-  console.log("bye");
+  const req = {
+    id: id.value,
+    psword: psword.value,
+  };
+  
+  fetch('/login',{
+    mothod: "post",
+    headers: {
+      'content-Type': 'application/json',
+    },
+    body: json.stringify(req)
+  });
 }

43e30d824d1bd951121091488510e95c16cdc8ca
won <whddnjsdlrk@naver.com>
2023-05-28 21:07:44 +0900

    Login, Sign-Up

--- login-lecture/app/src/routes/home/home.ctrl.js
+++ login-lecture/app/src/routes/home/home.ctrl.js
@@ -5,11 +5,13 @@ const User = require('../../models/User');
 const output = {
   home: (req,res)=>{
     res.render('home/index');
-  },
-  
+  },  
   login: (req,res) =>{
     res.render('home/login');
   },
+  register: (req,res) => {
+    res.render('home/register');
+  }
 };
 
 const process = {

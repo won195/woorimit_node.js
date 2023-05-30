8be35ec00deac04eb348233e4d96565f81470ef4
won <whddnjsdlrk@naver.com>
2023-05-30 22:53:01 +0900

    Implementation of login function through fs module

--- login-lecture/app/src/models/UserStorage.js
+++ login-lecture/app/src/models/UserStorage.js
@@ -1,14 +1,10 @@
 'use strict';
 
-class UserStorage {
-  static #users = {
-    id: ['wdwd','123123','123'],
-    psword: ['123','123','123'],
-    name: ['a','b','c'],
-  };
+const fs = require("fs");
 
+class UserStorage {
   static getUsers(...fields) {
-    const users = this.#users;
+    // const users = this.#users;
     const newUsers = fields.reduce((newUsers, field) => {
       if (users.hasOwnProperty(field)) {
         newUsers[field] = users[field];
@@ -20,19 +16,27 @@ class UserStorage {
   }
 
   static getUserInfo(id) {
-    const users = this.#users;
+    return fs.readFile("./src/databases/users.json")
+      .then((data) => {
+        return this.#getUserInfo(data, id);
+      })
+      .catch(console.error);
+  }
+  
+  static #getUserInfo(){
+    const users = JSON.parse(data);
     const idx = users.id.indexOf(id);
     const usersKeys = Object.keys(users); // => [id, psword,name]
     const userInfo = usersKeys.reduce((newUser, info) => {
       newUser[info] = users[info][idx];
       return newUser;
     }, {});
-
+    
     return userInfo;
   }
-  
+
   static save(userInfo){
-    const users = this.#users;
+    // const users = this.#users;
     users.id.push(userInfo.id);
     users.name.push(userInfo.name);
     users.psword.push(userInfo.psword);
